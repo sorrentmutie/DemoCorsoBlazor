394f977452d7e41420ec369aa6b746934f47f2c3
salvatore <salvatore.sorrentino@live.com>
2023-11-03 15:58:35 +0100

    Chart

---
+++ DemoCorsoBlazor.Library/Shared/Series.cs
@@ -0,0 +1,29 @@
+﻿using DemoCorsoBlazor.Library.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DemoCorsoBlazor.Library.Shared;
+
+public class Series : ComponentBase, IDisposable
+{
+    [Parameter] public List<double> Values { get; set; }
+    [CascadingParameter] public Chart ChartFather { get; set; }
+
+    private SeriesData seriesData = new SeriesData();
+
+
+    protected override void OnInitialized()
+    {
+        ChartFather.ChartData.Series = new List<SeriesData>();
+    }
+
+    protected override void OnParametersSet()
+    {
+        seriesData.Data = Values;
+        ChartFather?.ChartData.Series.Add(seriesData);
+    }
+
+    public void Dispose()
+    {
+        ChartFather.ChartData.Series.Remove(seriesData);
+    }
+}
